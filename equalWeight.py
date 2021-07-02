f3c950738a9312c1612fbb5dd902f561fb258866
Happollyon <36013973+Happollyon@users.noreply.github.com>
2021-07-02 23:34:51 +0100

    half done

--- equalWeight.py
+++ equalWeight.py
@@ -6,3 +6,31 @@ import pandas as pd
 import requests as r
 import math
 
+stocks = pd.read_csv('sp_500_stocks.csv',nrows=10)
+from secret import IEX_TOKEN
+
+
+# Adding our scks price to a pandas DataFrame
+my_columns = ['Ticker', 'Stock Price','Market Cap', 'Number of shares to buy']
+final_dataframe = pd.DataFrame(columns = my_columns)
+
+for stock in stocks['Ticker']:
+    api_url = f'https://sandbox.iexapis.com/stable/stock/{stock}/quote/?token={IEX_TOKEN}'  
+    data = r.get(api_url).json()
+    final_dataframe = final_dataframe.append(
+        pd.Series(
+            [
+             stock,
+             data['latestPrice'],
+             data['marketCap'],
+             'N/A'
+            ],
+            index = my_columns),
+            ignore_index=True
+            
+        
+        )
+print(final_dataframe)
+
+   
+
