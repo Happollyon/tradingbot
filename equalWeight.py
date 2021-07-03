950b4da757846a98303249a6ebc1288b9cd72c87
Happollyon <36013973+Happollyon@users.noreply.github.com>
2021-07-03 16:32:49 +0100

    added batch api calls to improve speed

--- equalWeight.py
+++ equalWeight.py
@@ -6,23 +6,38 @@ import pandas as pd
 import requests as r
 import math
 
-stocks = pd.read_csv('sp_500_stocks.csv',nrows=10)
+stocks = pd.read_csv('sp_500_stocks.csv')
 from secret import IEX_TOKEN
 
+# This functions splits the list into chuncks of 100
+def chunks(lst, n) :
+        for i in range(0, len(lst),n):
+            yield lst[i:i+n]
+
+
+symbol_groups = list(chunks(stocks['Ticker'],100))
+#now we transform all the stock in the lists into a string so we can pass in the url
+
+symbol_strings = []
+for i in range(0,len(symbol_groups)):
+    symbol_strings.append(','.join(symbol_groups[i]))
+    
 
 # Adding our scks price to a pandas DataFrame
 my_columns = ['Ticker', 'Stock Price','Market Cap', 'Number of shares to buy']
 final_dataframe = pd.DataFrame(columns = my_columns)
 
-for stock in stocks['Ticker']:
-    api_url = f'https://sandbox.iexapis.com/stable/stock/{stock}/quote/?token={IEX_TOKEN}'  
-    data = r.get(api_url).json()
-    final_dataframe = final_dataframe.append(
+for symbol_string in symbol_strings:
+    batch_api_url = f'https://sandbox.iexapis.com/stable/stock/market/batch?symbols={symbol_string},fb&types=quote&token={IEX_TOKEN}'
+    #api_url = f'https://sandbox.iexapis.com/stable/stock/{stock}/quote/?token={IEX_TOKEN}'  
+    data = r.get(batch_api_url).json()
+    for symbol in symbol_string.split(','):
+        final_dataframe = final_dataframe.append(
         pd.Series(
             [
-             stock,
-             data['latestPrice'],
-             data['marketCap'],
+             symbol,
+             data[symbol]['quote']['latestPrice'],
+             data[symbol]['quote']['marketCap'],
              'N/A'
             ],
             index = my_columns),
