f653b58cf674cb60d056be2010c35a1235e383a2
Fagner Nunes <fagnernunes11@gmail.com>
2021-09-03 23:27:21 +0100

    changes to market.py

--- crypto/market.py
+++ crypto/market.py
@@ -1,4 +1,5 @@
 """
+
 https://api.binance.com/api/v1/exchangeInfo    
 https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=1h
 """
@@ -20,11 +21,24 @@ for coin in newlist:
     symbol =coin['symbol']
     oneH=coin['price_change_percentage_1h_in_currency']
     two4H=coin['price_change_percentage_24h']
-    price = coin['current_price']
-    
-    print( f'{symbol}: price: {price} 1h: {oneH}% , 24h: {two4H} ')
+    price = coin['current_price']    
+   
+    if oneH > two4H:
+        difference = 'hour > 24'  
+    else:
+        difference = 'hour < 24'
+    print( f'{symbol}: price: {price} 1h: {oneH}% , 24h: {two4H} {difference}')
 
 current_coin_index = find(newlist,'symbol','btc')
-if current_coin_index < 99:
-    print(newlist[99]['price_change_percentage_1h_in_currency'],newlist[current_coin_index]['price_change_percentage_1h_in_currency'])
-print(current_coin_index)
+
+   
+for i in range(99,0,-1):
+    global new_symbol
+    new_symbol = newlist[i]['symbol'].upper()
+    new_symbol = symbol+'USDT'
+    response  = r.get(f'https://api.binance.com/api/v3/exchangeInfo?symbol={new_symbol}')
+    print(response.status_code)
+    if response.status_code==200:
+        print('symbol: ',newlist[i]['price_change_percentage_1h_in_currency'],'current_coin: ',newlist[current_coin_index]['price_change_percentage_1h_in_currency'])
+        break 
+
